38d216ca1187df52b6a8fbf021d4eb68eb057c41
scottbishop <sa_bishop@comcast.net>
2021-10-28 21:26:26 -0400

    routes

---
+++ src/routes/MainPage.tsx
@@ -0,0 +1,9 @@
+import { fetchMedicalData } from "../services/medicalAPIservice"
+import MedicalData from "../models/MedicalData";
+import { useEffect, useState } from "react";
+ 
+const MainPage = () => {
+    const [MedicalData, useMedicalData] = useState()
+}
+
+export default MainPage;
