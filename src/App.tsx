28d2e7941a08da4b93e5b12c3db1a8b5c09e5ff9
Kaela <kaelaagreen@gmail.com>
2021-11-01 21:52:37 -0400

    fixed results file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "./App.css";
+import Results from "./components/Results"
 import MainPage from "./routes/MainPage";
 
 function App() {
   return <div className="App">
     <MainPage />
+    <Results />
   </div>;
 }
 
